bdff5faaa0a66e39bad62a08835bb97c7ab752fe
tserings <tserings@berea.edu>
2025-01-13 11:40:34 -0500

    Added a simple camera prototype.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
     Edits made by:
     TODO: <Add name of student 1 Here> -- to be added by student 1
-    TODO: <Add name of student 2 Here> -- to be added by student 2
+    Sonam Tsering
     TODO: <Add name of student 3 Here> -- to be added by student 3 (if relevant otherwise, student 2 should remove this line)
  */
 #include <iostream>
@@ -20,13 +20,22 @@ int main() {
     cout  << " If not, see See https://en.wikipedia.org/wiki/Rubber_duck_debugging.\n" << endl;
     cout << " How many rubber ducks do you want to see? ";
     cin >> ducknum;
-
+    
     for (int i = 0; i < ducknum; i++) { 
         cout << "                __     " << endl; // TODO: student 1 may change this line and may add additional lines immediately above
         cout << "              <(o )___-" << endl; // student 3(1) may change this line and add additional lines between this line and immediately below
         cout << "               ( .__> /" << endl; // student 3(2) may change this line
         cout << "                `----' " << endl; // TODO student 2 may change this line and may add lines below
     }
+    
 
+        
+    for (int i = 0; i < ducknum; i++) {
+        cout << "                _______ " << endl; // TODO: student 2 may change this line and may add additional lines immediately above
+        cout << "              |  _ |__||" << endl; // student 3(1) may change this line and add additional lines between this line and immediately below
+        cout << "              |  |_|   |"<< endl; // student 3(2) may change this line
+        cout << "              |________| `----' " << endl; // TODO student 2 may change this line and may add lines below
+        cout << "              This is a camera prototype " << endl;
+    }
     return 0;
 }
